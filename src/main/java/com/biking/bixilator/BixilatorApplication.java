eea1cb335542100a72648f41dd7aaf0d1032c133
Marko <markolitovchenko24@gmail.com>
2022-11-20 23:21:11 -0500

    Created maven project

---
+++ src/main/java/com/biking/bixilator/BixilatorApplication.java
@@ -0,0 +1,13 @@
+package com.biking.bixilator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BixilatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BixilatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biking/bixilator/BixilatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biking.bixilator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BixilatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
