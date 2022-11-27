771d995283b4e359b7da6b5fe70cf436acd75d02
Marko <markolitovchenko24@gmail.com>
2022-11-26 19:30:26 -0500

    Added StationInfo entity with fields

--- src/main/java/com/biking/bixilator/entities/StationInfo.java
+++ src/main/java/com/biking/bixilator/entities/StationInfo.java
@@ -1,5 +1,19 @@
 package com.biking.bixilator.entities;
 
-public class StationInfo {
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class StationInfo {
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String name;
+    private int capacity;
+    private boolean isChargingStation;
+    private boolean hasKiosk;
 }
