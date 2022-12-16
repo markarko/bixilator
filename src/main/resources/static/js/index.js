0bea00fee6d40eeaec4b1d01c6f3ed8b7c863a8c
Marko <markolitovchenko24@gmail.com>
2022-12-15 22:17:39 -0500

    Added google maps and markers

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,8 @@
 let form = document.querySelector("#search-form");
+let map;
+let markers = [];
+
+document.querySelector("#map-script").setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
 
 form.addEventListener("submit", (event) => {
     let searchQuery = document.querySelector("#search-input").value;
@@ -12,12 +16,37 @@ form.addEventListener("submit", (event) => {
     })
         .then((res) => {
             return res.json();
-            //console.log(res);
         })
         .then((json) => {
+            for (let obj of json){
+                createMarker(new google.maps.LatLng(obj.latitude, obj.longitude), obj.name);
+            }
             console.log(json);
         })
         .catch((err) => {
             console.log(err);
         });
 });
+
+function initMap() {
+    let options = {
+        center: { lat: 45.539385081961676, lng: -73.54099988937377 },
+        zoom: 12
+    }
+    map = new google.maps.Map(document.getElementById("map"), options);
+}
+
+function createMarker(pos, title){
+    markers.push(
+    new google.maps.Marker({
+        position: pos,
+        map,
+        title: `${title}`,
+    }));
+}
+
+function clearMarkers(){
+    for(let marker of markers){
+        marker.setMap(null);
+    }
+}
